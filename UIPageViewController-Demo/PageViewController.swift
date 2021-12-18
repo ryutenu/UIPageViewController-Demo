4b95c81c6b455c030cc4711aa721b80ca1413dcd
Alan Liu <ryutenu.tokyo@gmail.com>
2021-12-18 22:57:29 +0900

    [Add] PageViewController

---
+++ UIPageViewController-Demo/PageViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PageViewController.swift
+//  UIPageViewController-Demo
+//
+//  Created by Alan Liu on 2021/12/18.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
