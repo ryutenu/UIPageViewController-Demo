c209ceac12300c9c1424d002b85cf53d082c2cfc
Alan Liu <ryutenu.tokyo@gmail.com>
2021-12-19 00:03:20 +0900

    [Mod] Added dataSource of the page VC

--- UIPageViewController-Demo/PageViewController.swift
+++ UIPageViewController-Demo/PageViewController.swift
@@ -9,8 +9,47 @@ import UIKit
 
 class PageViewController: UIPageViewController {
     
+    private var controllers: [UIViewController] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Set up page view controller
+        setupPageVC()
+    }
+    
+    // Set up page view controller
+    private func setupPageVC() {
+        guard let firstVC = storyboard?.instantiateViewController(withIdentifier: "FirstVC") as? FirstViewController,
+              let secondVC = storyboard?.instantiateViewController(withIdentifier: "SecondVC") as? SecondViewController,
+              let thirdVC = storyboard?.instantiateViewController(withIdentifier: "ThirdVC") as? ThirdViewController
+        else { return }
         
+        controllers = [firstVC, secondVC, thirdVC]
+        
+        setViewControllers([controllers[1]], direction: .forward, animated: false)
+        
+        dataSource = self
+    }
+}
+
+extension PageViewController: UIPageViewControllerDataSource {
+    // Swipe to right
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        if let index = controllers.firstIndex(of: viewController),
+           index < controllers.count - 1 {
+            return controllers[index + 1]
+        } else {
+            return nil
+        }
+    }
+    
+    // Swipe to left
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        if let index = controllers.firstIndex(of: viewController),
+           index > 0 {
+            return controllers[index - 1]
+        } else {
+            return nil
+        }
     }
 }
