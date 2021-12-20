b09ca65ad4ec74af647b799935184d420f48b60b
Alan Liu <ryutenu.tokyo@gmail.com>
2021-12-20 17:00:03 +0900

    [Mod] Minor fixes

--- UIPageViewController-Demo/PageViewController.swift
+++ UIPageViewController-Demo/PageViewController.swift
@@ -26,6 +26,8 @@ class PageViewController: UIPageViewController {
         
         controllers = [firstVC, secondVC, thirdVC]
         
+        guard controllers.indices.contains(1) else { return }
+        
         setViewControllers([controllers[1]], direction: .forward, animated: false)
         
         dataSource = self
@@ -33,21 +35,19 @@ class PageViewController: UIPageViewController {
 }
 
 extension PageViewController: UIPageViewControllerDataSource {
-    // Swipe to right
+    // Swipe to left
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        if let index = controllers.firstIndex(of: viewController),
-           index < controllers.count - 1 {
-            return controllers[index + 1]
+        if let index = controllers.firstIndex(of: viewController), index > 0, controllers.indices.contains(index - 1) {
+            return controllers[index - 1]
         } else {
             return nil
         }
     }
     
-    // Swipe to left
+    // Swipe to right
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        if let index = controllers.firstIndex(of: viewController),
-           index > 0 {
-            return controllers[index - 1]
+        if let index = controllers.firstIndex(of: viewController), index < controllers.count - 1, controllers.indices.contains(index + 1) {
+            return controllers[index + 1]
         } else {
             return nil
         }
